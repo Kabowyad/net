0ee4e2dd41bb9d30bb9899de83cc58e31e50f430
kabowyad <kabowyad@gmail.com>
2017-09-28 21:34:29 +0500

    №3

---
+++ ConsoleApplication/Tests/Class2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using NUnit.Framework;
+
+namespace ConsoleApplication.Tests{
+    [TestFixture]
+    public class Class2{
+        [Test]
+        public void test(){ 
+            ICandy saltCandy = new SaltCandy();
+            ICandy caramelCandy = new CaramelCandy();
+            
+            Action<ICandy, ICandy> delegAction;
+            delegAction = saltC;
+            compare(saltCandy, caramelCandy, delegAction);
+
+            Func<ICandy, ICandy, ArrayList> sort = sortCandy;
+            sort(caramelCandy, saltCandy);
+
+        }
+        
+        static void compare(ICandy x, ICandy y, Action<ICandy, ICandy> delegAction){
+             if (x.GetCandyType().Length > y.GetCandyType().Length)
+                 delegAction(x, y);
+        }
+       
+        static void saltC(ICandy x, ICandy y){
+            Console.WriteLine("x > y");
+        }
+
+        static ArrayList sortCandy(ICandy x, ICandy y){
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(x);
+            arrayList.Add(y);
+            return arrayList;
+        }
+    }
+}
